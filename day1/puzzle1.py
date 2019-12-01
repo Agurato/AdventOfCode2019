565f7ec91a2beb96f0866b2b1b7f2a57a443eb19
Agurato <Agurato@users.noreply.github.com>
2019-12-01 16:27:04 +0100

    python day 1 modification
    Signed-off-by: Agurato <Agurato@users.noreply.github.com>

--- day1/puzzle1.py
+++ day1/puzzle1.py
@@ -3,7 +3,7 @@
 if __name__ == '__main__':
     with open("input.txt", "r") as input_f:
         fuel_sum = 0
-        for line in input_f.readlines():
-            fuel_sum += int(int(line) / 3) - 2
+        for line in input_f:
+            fuel_sum += int(line) // 3 - 2
         print("Total fuel sum (part 1) :")
         print(fuel_sum)
--- day1/puzzle2.py
+++ day1/puzzle2.py
@@ -2,7 +2,7 @@
 
 
 def calculate_fuel(mass):
-    fuel_mass = int(mass / 3) - 2
+    fuel_mass = mass // 3 - 2
     if fuel_mass <= 0:
         return 0
     return fuel_mass + calculate_fuel(fuel_mass)
@@ -11,7 +11,7 @@ def calculate_fuel(mass):
 if __name__ == '__main__':
     with open("input.txt", "r") as input_f:
         fuel_sum = 0
-        for line in input_f.readlines():
+        for line in input_f:
             fuel_sum += calculate_fuel(int(line))
         print("Total fuel sum (part 2) :")
         print(fuel_sum)
