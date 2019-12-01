5b5f8ec54939d64e9b958cb294965f6e18be5ec1
Agurato <Agurato@users.noreply.github.com>
2019-12-01 09:30:59 +0100

    Day 1
    Signed-off-by: Agurato <Agurato@users.noreply.github.com>

---
+++ day1/puzzle1.py
@@ -0,0 +1,9 @@
+# https://adventofcode.com/2019/day/1
+
+if __name__ == '__main__':
+    with open("input.txt", "r") as input_f:
+        fuel_sum = 0
+        for line in input_f.readlines():
+            fuel_sum += int(int(line) / 3) - 2
+        print("Total fuel sum (part 1) :")
+        print(fuel_sum)
