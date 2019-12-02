4e058cb7e91fa613ee41872393ba5a64452c4cb7
uj <aulien.jbadji@gmail.com>
2019-12-02 15:49:36 +0100

    add rust day2 and separate files

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-fn fetch_input(path: &str) -> Result<Vec<i32>, &'static str> {
+pub fn fetch_input(path: &str) -> Result<Vec<i32>, &'static str> {
     let mut file = match File::open(path) {
         Err(_) => return Err("Error with filepath"),
         Ok(f) => f,
@@ -18,11 +18,11 @@ fn fetch_input(path: &str) -> Result<Vec<i32>, &'static str> {
     Ok(commands)
 }
 
-fn step_1a(items: &Vec<i32>) -> i32 {
+pub fn step_1a(items: &Vec<i32>) -> i32 {
     items.iter().map(|x| (x / 3) - 2).sum()
 }
 
-fn step_1b(items: &Vec<i32>) -> i32 {
+pub fn step_1b(items: &Vec<i32>) -> i32 {
     let get_fuel_per_module = |mass| {
         let mut supplementary_fuel: i32 = (mass / 3) - 2;
         let mut total_fuel = supplementary_fuel;
@@ -36,14 +36,5 @@ fn step_1b(items: &Vec<i32>) -> i32 {
         total_fuel
     };
 
-    items
-        .iter()
-        .map(|x| get_fuel_per_module(x))
-        .sum()
-}
-
-fn main() {
-    let items = fetch_input("res/1.txt").expect("Could not open file");
-    println!("Step 1: {}", step_1a(&items));
-    println!("Step 1b: {}", step_1b(&items));
+    items.iter().map(|x| get_fuel_per_module(x)).sum()
 }
