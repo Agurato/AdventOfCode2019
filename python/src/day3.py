4504f5524c933361699178fbdd6c81625b5a6bf6
Vincent Monot <Agurato@users.noreply.github.com>
2019-12-03 13:23:37 +0100

    python day3 using yield

--- python/src/day3.py
+++ python/src/day3.py
@@ -1,37 +1,26 @@
 # https://adventofcode.com/2019/day/3
 
 
-def check_intersec(line, point1, point2):
-   for i in range(len(line)-1):
-      if point1[0] == point2[0] and line[i][1] == line[i+1][1]:
-         if min(point1[1], point2[1]) < line[i][1] and max(point1[1], point2[1]) > line[i][1] and min(line[i][0], line[i+1][0]) < point1[0] and max(line[i][0], line[i+1][0]) > point1[0]:
-            #print(f"intersec [{point1}, {point2}] with [{line[i]}, {line[i+1]}] at ({point1[0]}, {line[i][1]})")
-            return abs(point1[0]) + abs(line[i][1])
-      elif point1[1] == point2[1] and line[i][0] == line[i+1][0]:
-         if min(point1[0], point2[0]) < line[i][0] and max(point1[0], point2[0]) > line[i][0] and min(line[i][1], line[i+1][1]) < point1[1] and max(line[i][1], line[i+1][1]) > point1[1]:
-            #print(f"intersec [{point1}, {point2}] with [{line[i]}, {line[i+1]}] at ({point1[1]}, {line[i][0]})")
-            return abs(point1[1]) + abs(line[i][0])
-        
-   return -1
-
-
-def puzzle1(input_f):
-   line1 = [(0,0)]
+def get_wire(line):
+   wire = [(0,0)]
    count = 0
-   min_dist = 0
-   for instruction in input_f.readline().split(","):
+   for instruction in line.split(","):
       direction = instruction[0]
       if direction == "U":
-         line1.append((line1[count][0],line1[count][1]+int(instruction[1:])))
+         wire.append((wire[count][0],wire[count][1]+int(instruction[1:])))
       if direction == "D":
-         line1.append((line1[count][0],line1[count][1]-int(instruction[1:])))
+         wire.append((wire[count][0],wire[count][1]-int(instruction[1:])))
       if direction == "L":
-         line1.append((line1[count][0]-int(instruction[1:]),line1[count][1]))
+         wire.append((wire[count][0]-int(instruction[1:]),wire[count][1]))
       if direction == "R":
-         line1.append((line1[count][0]+int(instruction[1:]),line1[count][1]))
+         wire.append((wire[count][0]+int(instruction[1:]),wire[count][1]))
       count += 1
+   return wire
+
+
+def get_segment(line2):
    prev_point = (0,0)
-   for instruction in input_f.readline().split(","):
+   for instruction in line2.split(","): 
       direction = instruction[0]
       curr_point = (0,0)
       if direction == "U":
@@ -43,17 +32,44 @@ def puzzle1(input_f):
       if direction == "R":
          curr_point = (prev_point[0]+int(instruction[1:]), prev_point[1])
       
-      # check intersection
-      dist = check_intersec(line1, prev_point, curr_point)
+      yield (prev_point, curr_point)
+      prev_point = curr_point[:]
+   return
+      
+
+def check_intersec(wire, segment):
+   for i in range(len(wire)-1):
+      if segment[0][0] == segment[1][0] and wire[i][1] == wire[i+1][1]:
+         if min(segment[0][1], segment[1][1]) < wire[i][1] and max(segment[0][1], segment[1][1]) > wire[i][1] and min(wire[i][0], wire[i+1][0]) < segment[0][0] and max(wire[i][0], wire[i+1][0]) > segment[0][0]:
+            #print(f"intersec [{segment[0]}, {segment[1]}] with [{wire[i]}, {wire[i+1]}] at ({segment[0][0]}, {wire[i][1]})")
+            return abs(segment[0][0]) + abs(wire[i][1])
+      elif segment[0][1] == segment[1][1] and wire[i][0] == wire[i+1][0]:
+         if min(segment[0][0], segment[1][0]) < wire[i][0] and max(segment[0][0], segment[1][0]) > wire[i][0] and min(wire[i][1], wire[i+1][1]) < segment[0][1] and max(wire[i][1], wire[i+1][1]) > segment[0][1]:
+            #print(f"intersec [{segment[0]}, {segment[1]}] with [{wire[i]}, {wire[i+1]}] at ({segment[0][1]}, {wire[i][0]})")
+            return abs(segment[0][1]) + abs(wire[i][0])
+        
+   return -1
+
+
+def puzzle1(input_f):
+   wire1 = get_wire(input_f.readline())
+   min_dist = 0
+   for segment in get_segment(input_f.readline()):
+      dist = check_intersec(wire1, segment)
       if dist != -1:
          if min_dist == 0:
             min_dist = dist
          else:
             min_dist = min(min_dist, dist)
-      prev_point = curr_point[:]
    return min_dist
 
 
+def puzzle2(input_f):
+   wire1 = get_wire(input_f.readline())
+
+
 if __name__ == "__main__":
-   with open("res/day3.txt") as input_f:
+   with open("day3.txt") as input_f:
       print(puzzle1(input_f))
+      input_f.seek(0)
+      print(puzzle2(input_f))
