f45c3e0621709032c72b1781a54ec9ceaac4dcc1
Vincent Monot <Agurato@users.noreply.github.com>
2019-12-03 15:05:46 +0100

    day3 Point class

--- python/src/day3.py
+++ python/src/day3.py
@@ -1,78 +1,80 @@
 # https://adventofcode.com/2019/day/3
 
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
 def get_wire(line):
-    wire = [(0, 0)]
+    wire = [Point(0, 0)]
     count = 0
     for instruction in line.split(","):
         direction = instruction[0]
         if direction == "U":
-            wire.append((wire[count][0], wire[count][1] + int(instruction[1:])))
+            wire.append(Point(wire[count].x, wire[count].y + int(instruction[1:])))
         if direction == "D":
-            wire.append((wire[count][0], wire[count][1] - int(instruction[1:])))
+            wire.append(Point(wire[count].x, wire[count].y - int(instruction[1:])))
         if direction == "L":
-            wire.append((wire[count][0] - int(instruction[1:]), wire[count][1]))
+            wire.append(Point(wire[count].x - int(instruction[1:]), wire[count].y))
         if direction == "R":
-            wire.append((wire[count][0] + int(instruction[1:]), wire[count][1]))
+            wire.append(Point(wire[count].x + int(instruction[1:]), wire[count].y))
         count += 1
     return wire
 
 
 def get_segment(line):
-    prev_point = (0, 0)
+    prev_point = Point(0, 0)
     for instruction in line.split(","):
         direction = instruction[0]
-        curr_point = (0, 0)
+        curr_point = Point(0, 0)
         if direction == "U":
-            curr_point = (prev_point[0], prev_point[1] + int(instruction[1:]))
+            curr_point = Point(prev_point.x, prev_point.y + int(instruction[1:]))
         if direction == "D":
-            curr_point = (prev_point[0], prev_point[1] - int(instruction[1:]))
+            curr_point = Point(prev_point.x, prev_point.y - int(instruction[1:]))
         if direction == "L":
-            curr_point = (prev_point[0] - int(instruction[1:]), prev_point[1])
+            curr_point = Point(prev_point.x - int(instruction[1:]), prev_point.y)
         if direction == "R":
-            curr_point = (prev_point[0] + int(instruction[1:]), prev_point[1])
+            curr_point = Point(prev_point.x + int(instruction[1:]), prev_point.y)
 
         yield (prev_point, curr_point)
-        prev_point = curr_point[:]
+        prev_point = Point(curr_point.x, curr_point.y)
     return
 
 
 def get_intersec(wire, segment):
     wire_steps = 0
     for i in range(len(wire) - 1):
-        if segment[0][0] == segment[1][0] and wire[i][1] == wire[i + 1][1]:
+        if segment[0].x == segment[1].x and wire[i].y == wire[i + 1].y:
             if (
-                min(segment[0][1], segment[1][1]) < wire[i][1]
-                and max(segment[0][1], segment[1][1]) > wire[i][1]
-                and min(wire[i][0], wire[i + 1][0]) < segment[0][0]
-                and max(wire[i][0], wire[i + 1][0]) > segment[0][0]
+                min(segment[0].y, segment[1].y) < wire[i].y
+                and max(segment[0].y, segment[1].y) > wire[i].y
+                and min(wire[i].x, wire[i + 1].x) < segment[0].x
+                and max(wire[i].x, wire[i + 1].x) > segment[0].x
             ):
                 return (
-                    segment[0][0],
-                    wire[i][1],
-                    wire_steps + abs(segment[0][0] - wire[i][0]),
+                    Point(segment[0].x, wire[i].y),
+                    wire_steps + abs(segment[0].x - wire[i].x),
                 )
-        elif segment[0][1] == segment[1][1] and wire[i][0] == wire[i + 1][0]:
+        elif segment[0].y == segment[1].y and wire[i].x == wire[i + 1].x:
             if (
-                min(segment[0][0], segment[1][0]) < wire[i][0]
-                and max(segment[0][0], segment[1][0]) > wire[i][0]
-                and min(wire[i][1], wire[i + 1][1]) < segment[0][1]
-                and max(wire[i][1], wire[i + 1][1]) > segment[0][1]
+                min(segment[0].x, segment[1].x) < wire[i].x
+                and max(segment[0].x, segment[1].x) > wire[i].x
+                and min(wire[i].y, wire[i + 1].y) < segment[0].y
+                and max(wire[i].y, wire[i + 1].y) > segment[0].y
             ):
                 return (
-                    wire[i][0],
-                    segment[0][1],
-                    wire_steps + abs(segment[0][1] - wire[i][1]),
+                    Point(wire[i].x, segment[0].y),
+                    wire_steps + abs(segment[0].y - wire[i].y),
                 )
-        wire_steps += abs(wire[i + 1][0] - wire[i][0]) + abs(
-            wire[i + 1][1] - wire[i][1]
-        )
+        wire_steps += abs(wire[i + 1].x - wire[i].x) + abs(wire[i + 1].y - wire[i].y)
 
     return
 
 
 def manhattan(point1, point2):
-    return abs(point2[0] - point1[0]) + abs(point2[1] - point1[1])
+    return abs(point2.x - point1.x) + abs(point2.y - point1.y)
 
 
 def puzzle1(input_f):
@@ -81,7 +83,7 @@ def puzzle1(input_f):
     for segment in get_segment(input_f.readline()):
         intersec = get_intersec(wire1, segment)
         if intersec is not None:
-            dist = manhattan((0, 0), intersec[0:2])
+            dist = manhattan(Point(0, 0), intersec[0])
             if min_dist == 0:
                 min_dist = dist
             else:
@@ -96,7 +98,7 @@ def puzzle2(input_f):
     for segment in get_segment(input_f.readline()):
         intersec = get_intersec(wire1, segment)
         if intersec is not None:
-            steps = wire2_steps + manhattan(segment[0], intersec[0:2]) + intersec[2]
+            steps = wire2_steps + manhattan(segment[0], intersec[0]) + intersec[1]
             if min_steps == 0:
                 min_steps = steps
             else:
