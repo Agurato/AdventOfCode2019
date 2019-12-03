38476a45a3fe6c9693f1f67eb23a546e9dacf7ce
uj <sudo.screenlife@gmail.com>
2019-12-03 11:38:40 +0100

    split into 2 functions

--- rust/src/day3.rs
+++ rust/src/day3.rs
@@ -5,7 +5,8 @@ use std::io::prelude::*;
     From instructions, construct a list of segments belonging to
     wire1 and wire2, then check for collisions between every segment.
 */
-pub fn fetch_input(path: &str) -> Result<Vec<Vec<(i32, i32)>>, &'static str> {
+
+pub fn fetch_input(path: &str) -> Result<Vec<Vec<String>>, &'static str> {
     let mut file = match File::open(path) {
         Err(_) => return Err("Error with filepath"),
         Ok(f) => f,
@@ -16,13 +17,20 @@ pub fn fetch_input(path: &str) -> Result<Vec<Vec<(i32, i32)>>, &'static str> {
     file.read_to_string(&mut contents)
         .expect("failed to read string");
 
-    let wires: Vec<Vec<&str>> = contents
+    let wires: Vec<Vec<String>> = contents
         .split("\n")
-        .map(|x| x.split(",").collect())
+        .map(|x| x.split(",").map(|s| s.to_string()).collect())
         .collect();
 
-    let mut wire_segment_lists: Vec<Vec<(i32, i32)>> = Vec::new();
+    Ok(wires)
+}
+
+pub fn get_segments(wires: Vec<Vec<String>>) -> Result<Vec<Vec<(i32, i32)>>, &'static str> {
+    let mut wire_segments: Vec<Vec<(i32, i32)>> = Vec::new();
     let mut tmp;
+    let mut carry = (0i32, 0i32);
+
+    // Transform U30 into (0, 30)
     for wire in &wires {
         tmp = wire
             .iter()
@@ -37,11 +45,14 @@ pub fn fetch_input(path: &str) -> Result<Vec<Vec<(i32, i32)>>, &'static str> {
                 };
             })
             .collect();
-        wire_segment_lists.push(tmp);
+        wire_segments.push(tmp);
     }
-    println!("{:?}", wire_segment_lists[0]);
-    println!("{:?}", wire_segment_lists[1]);
-    Ok(wire_segment_lists)
+
+    //Apply displacements sequentially
+
+    println!("{:?}", wire_segments[0]);
+    println!("{:?}", wire_segments[1]);
+    Ok(wire_segments)
 }
 
 #[cfg(test)]
