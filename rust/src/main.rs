57277ee6617f5e7292cb1875b9161b6da0f18d40
uj <aulien.jbadji@gmail.com>
2019-12-05 19:12:44 +0100

    day5 step1

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,24 +2,27 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() {
-    let items_1 = day1::fetch_input("res/1.txt").expect("Could not open file");
-    println!("Step 1a: {}", day1::step_1a(&items_1));
-    println!("Step 1b: {}", day1::step_1b(&items_1));
+    // let items_1 = day1::fetch_input("res/1.txt").expect("Could not open file");
+    // println!("Step 1a: {}", day1::step_1a(&items_1));
+    // println!("Step 1b: {}", day1::step_1b(&items_1));
 
-    let mut items_2 = day2::fetch_input("res/2.txt").expect("Could not open file");
-    items_2[1] = 12;
-    items_2[2] = 2;
-    println!("Step 2a: {}", day2::step_2a(&mut items_2));
-    items_2 = day2::fetch_input("res/2.txt").expect("Could not open file");
-    println!("Step 2b: {}", day2::step_2b(&mut items_2).expect("oops"));
+    // let mut items_2 = day2::fetch_input("res/2.txt").expect("Could not open file");
+    // items_2[1] = 12;
+    // items_2[2] = 2;
+    // println!("Step 2a: {}", day2::step_2a(&mut items_2));
+    // items_2 = day2::fetch_input("res/2.txt").expect("Could not open file");
+    // println!("Step 2b: {}", day2::step_2b(&mut items_2).expect("oops"));
 
-    let items_3 = day3::fetch_input("res/3.txt").expect("Could not open file");
-    println!("Step 3a: {}", day3::step_3a(&items_3));
+    // let items_3 = day3::fetch_input("res/3.txt").expect("Could not open file");
+    // println!("Step 3a: {}", day3::step_3a(&items_3));
 
-    let items_4 = (172851, 675869);
+    // let items_4 = (172851, 675869);
+    // println!("Step 4a: {}", day4::step_4a(items_4.0, items_4.1));
+    // println!("Step 4b: {}", day4::step_4b(items_4.0, items_4.1));
 
-    println!("Step 4a: {}", day4::step_4a(items_4.0, items_4.1));
-    println!("Step 4b: {}", day4::step_4b(items_4.0, items_4.1));
+    let mut items_5 = day5::fetch_input("res/5.txt").expect("Could not open file");
+    day5::step_5a(&mut items_5, 1);
 }
