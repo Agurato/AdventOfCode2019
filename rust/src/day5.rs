39eaa04349cacc5728c0a8256e6b6909ab6e194c
uj <aulien.jbadji@gmail.com>
2019-12-05 19:15:12 +0100

    fmt

--- rust/src/day5.rs
+++ rust/src/day5.rs
@@ -22,38 +22,36 @@ pub fn step_5a(items: &mut Vec<i32>, input: i32) {
     let mut ip = 0;
 
     loop {
-        let op : String= format!("{:05}", items[ip]);
+        let op: String = format!("{:05}", items[ip]);
         let opcode = &op[3..];
         let params = op[..3].chars().rev();
 
-        let mut addrs : Vec<usize> = Vec::with_capacity(3);
+        let mut addrs: Vec<usize> = Vec::with_capacity(3);
 
-        if ip < items.len()-1 {
+        if ip < items.len() - 1 {
             for (param, offset) in params.zip(1..4) {
-                    match param {
-                        '0' => addrs.push(items[ip+offset] as usize), //position mode: the number corresponds to the address of the value
-                        '1' => addrs.push(ip+offset as usize), //immediate mode: the number corresponds to itself. We provide the address of the number.
-                        _ => panic!("unknown mode")
-                    }
+                match param {
+                    '0' => addrs.push(items[ip + offset] as usize), //position mode: the number corresponds to the address of the value
+                    '1' => addrs.push(ip + offset as usize), //immediate mode: the number corresponds to itself. We provide the address of the number.
+                    _ => panic!("unknown mode"),
+                }
             }
         }
 
-        let (result, ip_jump) : (Option<i32>, usize) = match opcode {
+        let (result, ip_jump): (Option<i32>, usize) = match opcode {
             "01" => {
-                items[addrs[2]] = items[addrs[0]] + items[addrs[1]]; 
+                items[addrs[2]] = items[addrs[0]] + items[addrs[1]];
                 (None, 4)
-            },
+            }
             "02" => {
-                items[addrs[2]] = items[addrs[0]] * items[addrs[1]]; 
+                items[addrs[2]] = items[addrs[0]] * items[addrs[1]];
                 (None, 4)
             }
             "03" => {
                 items[addrs[0]] = input;
                 (None, 2)
-                }
-            "04" => {
-                (Some(items[addrs[0]]), 2)
             }
+            "04" => (Some(items[addrs[0]]), 2),
             "99" => (None, 0),
             _ => panic!("invalid opcode {}", op),
         };
@@ -62,10 +60,10 @@ pub fn step_5a(items: &mut Vec<i32>, input: i32) {
             println!("DONE.");
             return ();
         }
-        
+
         match result {
             Some(r) => println!("OUTPUT: {}", r),
-            None => ()
+            None => (),
         };
 
         ip += ip_jump;
@@ -77,10 +75,9 @@ mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
-
     #[test]
     fn test_5a() {
-        let mut program = vec![1002,4,3,4,33];
+        let mut program = vec![1002, 4, 3, 4, 33];
         let input = 3;
         step_5a(&mut program, input);
 
